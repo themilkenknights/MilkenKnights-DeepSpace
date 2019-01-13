354f17093ceb8dc1232254ec202888e71d645223
Alexander Swerdlow <aswerdlow1@gmail.com>
2019-01-12 17:49:21 -0800

    Update

--- src/main/java/frc/robot/lib/vision/LimelightTarget.java
+++ src/main/java/frc/robot/lib/vision/LimelightTarget.java
@@ -2,12 +2,12 @@ package frc.robot.lib.vision;
 
 public class LimelightTarget {
 
+	public static LimelightTarget EMPTY = new LimelightTarget(false, 0.0, 0.0, 0.0, 0.0, 0.0);
 	private boolean validTarget;//Does limelight have a target
 	private double xOffset; //Horizontal center of bounding box from center in degrees
 	private double yOffset; //Vertical center of bounding box from center in degrees
 	private double area; //Pixel area of bounding box
 	private double captureTime; //Capture Time in Sec (Timer.FPGA)
-	public static LimelightTarget EMPTY = new LimelightTarget(false, 0.0,0.0,0.0,0.0,0.0);
 
 	public LimelightTarget(boolean validTarget, double xOffset, double yOffset, double horizLength, double vertLength, double captureTime) {
 		this.validTarget = validTarget;
