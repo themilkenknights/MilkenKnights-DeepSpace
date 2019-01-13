7e8a46f3023aac53f212a8fadeeaed3f2f5fe096
Alexander Swerdlow <aswerdlow1@gmail.com>
2019-01-12 18:51:52 -0800

    Update

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,9 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project. */
 /*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -19,101 +18,104 @@ import frc.robot.subsystems.Drive.DriveControlState;
 import frc.robot.subsystems.Input;
 import frc.robot.subsystems.SubsystemManager;
 import frc.robot.subsystems.Superstructure;
-
 import java.util.Arrays;
 
 public class Robot extends TimedRobot {
-		public static MatchState mMatchState = MatchState.DISABLED;
-		private final SubsystemManager mSubsystemManager = new SubsystemManager(Arrays.asList(Drive.getInstance(), Superstructure.getInstance(), Input.getInstance()));
-		private Looper mEnabledLooper = new Looper();
+	public static MatchState mMatchState = MatchState.DISABLED;
+	private final SubsystemManager mSubsystemManager = new SubsystemManager(Arrays.asList(Drive.getInstance(), Superstructure.getInstance(), Input.getInstance()));
+	private Looper mEnabledLooper = new Looper();
 
-		public Robot() {
-				CrashTracker.logRobotConstruction();
-		}
+	public Robot() { CrashTracker.logRobotConstruction(); }
 
-		@Override public void robotInit() {
-				try {
-						CrashTracker.logRobotInit();
-						mMatchState = MatchState.DISABLED;
-						mSubsystemManager.registerEnabledLoops(mEnabledLooper);
-						TrajectoryGenerator.getInstance().generateTrajectories();
-				} catch (Throwable t) {
-						CrashTracker.logThrowableCrash(t);
-						throw t;
-				}
+	@Override
+	public void robotInit() {
+		try {
+			CrashTracker.logRobotInit();
+			mMatchState = MatchState.DISABLED;
+			mSubsystemManager.registerEnabledLoops(mEnabledLooper);
+			TrajectoryGenerator.getInstance().generateTrajectories();
+		} catch (Throwable t) {
+			CrashTracker.logThrowableCrash(t);
+			throw t;
 		}
+	}
 
-		@Override public void disabledInit() {
-				try {
-						CrashTracker.logDisabledInit();
-						mMatchState = MatchState.DISABLED;
-						mEnabledLooper.stop();
-						AutoChooser.disableAuto();
-				} catch (Throwable t) {
-						CrashTracker.logThrowableCrash(t);
-						throw t;
-				}
+	@Override
+	public void disabledInit() {
+		try {
+			CrashTracker.logDisabledInit();
+			mMatchState = MatchState.DISABLED;
+			mEnabledLooper.stop();
+			AutoChooser.disableAuto();
+		} catch (Throwable t) {
+			CrashTracker.logThrowableCrash(t);
+			throw t;
 		}
+	}
 
-		@Override public void autonomousInit() {
-				try {
-						CrashTracker.logAutoInit();
-						mMatchState = MatchState.AUTO;
-						mEnabledLooper.start();
-						RobotState.getInstance().reset(Timer.getFPGATimestamp(), Pose2d.identity());
-						AutoChooser.startAuto();
-				} catch (Throwable t) {
-						CrashTracker.logThrowableCrash(t);
-						throw t;
-				}
+	@Override
+	public void autonomousInit() {
+		try {
+			CrashTracker.logAutoInit();
+			mMatchState = MatchState.AUTO;
+			mEnabledLooper.start();
+			RobotState.getInstance().reset(Timer.getFPGATimestamp(), Pose2d.identity());
+			AutoChooser.startAuto();
+		} catch (Throwable t) {
+			CrashTracker.logThrowableCrash(t);
+			throw t;
 		}
+	}
 
-		@Override public void teleopInit() {
-				try {
-						CrashTracker.logTeleopInit();
-						mMatchState = MatchState.TELEOP;
-						Drive.getInstance().mDriveControlState = DriveControlState.OPEN_LOOP;
-						mEnabledLooper.start();
-				} catch (Throwable t) {
-						CrashTracker.logThrowableCrash(t);
-						throw t;
-				}
+	@Override
+	public void teleopInit() {
+		try {
+			CrashTracker.logTeleopInit();
+			mMatchState = MatchState.TELEOP;
+			Drive.getInstance().mDriveControlState = DriveControlState.OPEN_LOOP;
+			mEnabledLooper.start();
+		} catch (Throwable t) {
+			CrashTracker.logThrowableCrash(t);
+			throw t;
 		}
+	}
 
-		@Override public void testInit() {
-				try {
-						mMatchState = MatchState.TEST;
-						mEnabledLooper.start();
-						System.out.println("Starting check systems.");
-						mEnabledLooper.stop();
-						Drive.getInstance().checkSystem();
-				} catch (Throwable t) {
-						CrashTracker.logThrowableCrash(t);
-						throw t;
-				}
+	@Override
+	public void testInit() {
+		try {
+			mMatchState = MatchState.TEST;
+			mEnabledLooper.start();
+			System.out.println("Starting check systems.");
+			mEnabledLooper.stop();
+			Drive.getInstance().checkSystem();
+		} catch (Throwable t) {
+			CrashTracker.logThrowableCrash(t);
+			throw t;
 		}
+	}
 
-		@Override public void robotPeriodic() {
-				try {
-						mSubsystemManager.outputToSmartDashboard();
-						mEnabledLooper.outputToSmartDashboard();
-						RobotState.getInstance().outputToSmartDashboard();
-				} catch (Throwable t) {
-						CrashTracker.logThrowableCrash(t);
-						throw t;
-				}
+	@Override
+	public void robotPeriodic() {
+		try {
+			mSubsystemManager.outputToSmartDashboard();
+			mEnabledLooper.outputToSmartDashboard();
+			RobotState.getInstance().outputToSmartDashboard();
+		} catch (Throwable t) {
+			CrashTracker.logThrowableCrash(t);
+			throw t;
 		}
+	}
 
-		@Override public void autonomousPeriodic() {
-		}
+	@Override
+	public void autonomousPeriodic() {}
 
-		@Override public void teleopPeriodic() {
-		}
+	@Override
+	public void teleopPeriodic() {}
 
-		@Override public void testPeriodic() {
-		}
+	@Override
+	public void testPeriodic() {}
 
-		public enum MatchState {
-				AUTO, TELEOP, DISABLED, TEST
-		}
+	public enum MatchState {
+		AUTO, TELEOP, DISABLED, TEST
+	}
 }
--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -7,90 +7,76 @@ import frc.robot.Robot;
 import frc.robot.lib.structure.ILooper;
 import frc.robot.lib.structure.Loop;
 import frc.robot.lib.vision.*;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Superstructure extends Subsystem {
-		public PixySPI pixy1;
-		public HashMap<Integer, ArrayList<PixyPacket>> packets = new HashMap<Integer, ArrayList<PixyPacket>>();
-		public int w = 0;
-		Port port = Port.kOnboardCS0;
-		String print;
-		private LimeLight limeLight;
-		private LimelightTarget target;
+	public PixySPI pixy1;
+	public HashMap<Integer, ArrayList<PixyPacket>> packets = new HashMap<Integer, ArrayList<PixyPacket>>();
+	public int w = 0;
+	Port port = Port.kOnboardCS0;
+	String print;
+	private LimeLight limeLight;
+	private LimelightTarget target;
 
-		public static Superstructure getInstance() {
-				return InstanceHolder.mInstance;
-		}
+	public static Superstructure getInstance() { return InstanceHolder.mInstance; }
 
-		public void Superstructure() {
-				limeLight = new LimeLight();
-				target = LimelightTarget.EMPTY;
-				pixy1 = new PixySPI(new SPI(port), packets, new PixyException(print));
-		}
+	public void Superstructure() { limeLight = new LimeLight(); target = LimelightTarget.EMPTY; pixy1 = new PixySPI(new SPI(port), packets, new PixyException(print)); }
 
-		@Override public void outputTelemetry() {
-				SmartDashboard.putString("Robot State", Robot.mMatchState.toString());
-		}
+	@Override
+	public void outputTelemetry() { SmartDashboard.putString("Robot State", Robot.mMatchState.toString()); }
 
-		public void registerEnabledLoops(ILooper enabledLooper) {
-				enabledLooper.register(new Loop() {
-						@Override public void onStart(double timestamp) {
-								synchronized (Superstructure.this) {
-								}
-						}
+	public void registerEnabledLoops(ILooper enabledLooper) {
+		enabledLooper.register(new Loop() {
+			@Override
+			public void onStart(double timestamp) { synchronized (Superstructure.this) {} }
 
-						@Override public void onLoop(double timestamp) {
-								synchronized (Superstructure.this) {
-										updateLimelight();
-										testPixy();
-								}
-						}
-
-						@Override public void onStop(double timestamp) {
-						}
-				});
-		}
-
-		private synchronized void updateLimelight() {
-				target = limeLight.returnTarget();
-		}
-
-		private synchronized void testPixy() {
-				if (w == 500) {
-						w = 0;
-				} else {
-						w++;
-						return;
+			@Override
+			public void onLoop(double timestamp) {
+				synchronized (Superstructure.this) {
+					updateLimelight();
+					testPixy();
 				}
-				int ret = -1;
-				// Get the packets from the pixy.
-				try {
-						ret = pixy1.readPackets();
-				} catch (PixyException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-				}
-				SmartDashboard.putNumber("Pixy Vision: packets size: ", packets.size());
-				for (int i = 1; i <= PixySPI.PIXY_SIG_COUNT; i++) {
-						SmartDashboard.putString("Pixy Vision: Signature: ", Integer.toString(i));
-						SmartDashboard.putNumber("Pixy Vision: packet: " + i + ": size: ", packets.get(i).size());
-						// Loop through the packets for this signature.
-						for (int j = 0; j < packets.get(i).size(); j++) {
-								SmartDashboard.putNumber("Pixy Vision: " + i + ": X: ", packets.get(i).get(j).X);
-								SmartDashboard.putNumber("Pixy Vision: " + i + ": Y: ", packets.get(i).get(j).Y);
-								SmartDashboard.putNumber("Pixy Vision: " + i + ": Width: ", packets.get(i).get(j).Width);
-								SmartDashboard.putNumber("Pixy Vision: " + i + ": Height: ", packets.get(i).get(j).Height);
-						}
-				}
-		}
+			}
 
-		public synchronized LimelightTarget getTarget() {
-				return target;
-		}
+			@Override
+			public void onStop(double timestamp) {}
+		});
+	}
 
-		private static class InstanceHolder {
-				private static final Superstructure mInstance = new Superstructure();
+	private synchronized void updateLimelight() { target = limeLight.returnTarget(); }
+
+	private synchronized void testPixy() {
+		if (w == 500) {
+			w = 0;
+		} else {
+			w++;
+			return;
 		}
+		int ret = -1;
+		// Get the packets from the pixy.
+		try {
+			ret = pixy1.readPackets();
+		} catch (PixyException e) {
+			e.printStackTrace();
+		}
+		SmartDashboard.putNumber("Pixy Vision: packets size: ", packets.size());
+		for (int i = 1; i <= PixySPI.PIXY_SIG_COUNT; i++) {
+			SmartDashboard.putString("Pixy Vision: Signature: ", Integer.toString(i));
+			SmartDashboard.putNumber("Pixy Vision: packet: " + i + ": size: ", packets.get(i).size());
+			// Loop through the packets for this signature.
+			for (int j = 0; j < packets.get(i).size(); j++) {
+				SmartDashboard.putNumber("Pixy Vision: " + i + ": X: ", packets.get(i).get(j).X);
+				SmartDashboard.putNumber("Pixy Vision: " + i + ": Y: ", packets.get(i).get(j).Y);
+				SmartDashboard.putNumber("Pixy Vision: " + i + ": Width: ", packets.get(i).get(j).Width);
+				SmartDashboard.putNumber("Pixy Vision: " + i + ": Height: ", packets.get(i).get(j).Height);
+			}
+		}
+	}
+
+	public synchronized LimelightTarget getTarget() { return target; }
+
+	private static class InstanceHolder {
+		private static final Superstructure mInstance = new Superstructure();
+	}
 }
