51c87573978815f9627241562b3b2514d50b129b
Alexander Swerdlow <35648800+alexanderswerdlow@users.noreply.github.com>
2019-01-27 22:17:43 -0800

    Update

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import frc.robot.lib.pixy.Pixy;
 import frc.robot.lib.structure.Subsystem;
 import frc.robot.lib.util.MovingAverage;
 import frc.robot.lib.vision.LimeLight;
@@ -9,9 +10,13 @@ import frc.robot.lib.vision.LimelightTarget;
 public class Vision extends Subsystem {
 		private LimeLight limeLight;
 		private MovingAverage mThrottleAverage = new MovingAverage(5);
-
+		static final int FRAME_WIDTH = 320, FRAME_HEIGHT = 200;
+		static final int MAX_BLOCKS = 10;
+		Pixy pixy1;
 		private Vision() {
-				limeLight = new LimeLight();
+			limeLight = new LimeLight();
+			pixy1 = new Pixy(0x053C3165);
+			Pixy.ensureAvailable(0x053C3165);
 		}
 
 		public static Vision getInstance() {
@@ -25,18 +30,28 @@ public class Vision extends Subsystem {
 
 		@Override
 		public void onStart(double timestamp) {
-				limeLight.setLEDMode(LimeLightControlMode.LedMode.kforceOn);
+			limeLight.setLEDMode(LimeLightControlMode.LedMode.kforceOn);
+			pixy1.setAutoWhiteBalance(false);
+			pixy1.setWhiteBalanceValue(new Pixy.WhiteBalanceSetting(0xA0, 0x80, 0x80));
+			pixy1.setAutoExposure(false);
+			pixy1.setExposureCompensation(new Pixy.ExposureSetting(10, 150));
+			pixy1.startBlockProgram();
+			pixy1.startFrameGrabber();
 		}
 
 		@Override
 		public void onLoop(double timestamp) {
 				mThrottleAverage.addNumber(limeLight.returnTarget());
+				System.out.println(pixy1.getBlocks().get(0).x);
 		}
 
 		@Override
 		public void onStop(double timestamp) {
 				mThrottleAverage.clear();
 				limeLight.setLEDMode(LimeLightControlMode.LedMode.kforceOff);
+			pixy1.stopFrameGrabber();
+			pixy1.stopBlockProgram();
+			pixy1.setLEDBrightness(1000);
 		}
 
 		public synchronized LimelightTarget getAverageTarget() {
