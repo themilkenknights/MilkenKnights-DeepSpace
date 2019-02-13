d21dbe9df15ede26f9618db6b951ceab59af3ec9
Alexander Swerdlow <aswerdlow1@gmail.com>
2019-02-13 12:08:29 -0800

    Update MotionMagicPixy.java

--- src/main/java/frc/robot/auto/actions/MotionMagicPixy.java
+++ src/main/java/frc/robot/auto/actions/MotionMagicPixy.java
@@ -6,7 +6,6 @@ import frc.robot.Constants.VISION;
 import frc.robot.lib.util.DriveSignal;
 import frc.robot.lib.util.InterpolatingDouble;
 import frc.robot.lib.util.MkTime;
-import frc.robot.lib.vision.VisionState;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Vision;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
@@ -14,7 +13,6 @@ import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 public class MotionMagicPixy implements Action {
 
 	private MkTime expirationTimer;
-	private VisionState mLastVisionState = VisionState.EMPTY;
 
 	public MotionMagicPixy() {
 		expirationTimer = new MkTime();
@@ -33,6 +31,8 @@ public class MotionMagicPixy implements Action {
 		if (mDist > 5.0 && mDist < 60) {
 			double mSteer = DRIVE.kVisionTurnP * target.getX();
 			DriveSignal mSig = Drive.getInstance().setMotionMagicDeltaSetpoint(new DriveSignal(mDist, mDist, NeutralMode.Coast), new DriveSignal(mSteer, -mSteer));
+		} else {
+			Drive.getInstance().setOpenLoop(new DriveSignal(-0.3, -0.3));
 		}
 	}
 
